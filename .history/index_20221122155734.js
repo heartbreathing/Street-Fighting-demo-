e59bec0464199a0962c8817cca06981635118dc0
heartbreathing <li.wang@hyperisland.se>
2022-11-22 23:48:34 +0100

    Add background image and shop and idle

---
+++ .history/index_20221122155734.js
@@ -0,0 +1,236 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');//Draw a 2d animation context.
+
+canvas.width = 1024;
+canvas.height = 576;
+
+
+//Define the size of the context and use fillRect to draw the rectangle within color.
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.7;
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+});
+
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+});
+
+//Instance object of the class Fighter.
+const player = new Fighter({
+    position:{
+        x : 0,
+        y : 0
+    },
+    velocity: {
+        x : 0,
+        y : 10  
+    },
+    offset: {
+        x : 0,
+        y : 0 
+    },
+    color: 'red',
+    imageSrc: './img/shop.png',
+})
+
+
+//Instance object of the class Fighter.
+const enemy = new Fighter({
+    position:{
+        x :300,
+        y : 100
+    },
+    velocity: {
+        x : 0,
+        y : 10  
+    },
+    offset: {
+        x : -50,
+        y : 0 
+    },
+    color: 'pink'
+})
+
+
+//Declare initial keys.
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    s:{
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    }
+}
+
+//Create a lastKey to set the status of the last key.
+let lastKey;
+
+decreaseTimer();
+
+//Create a animate function to control the movement of the player and the enemy. 
+let gameOver = false;
+function animate() {
+
+    //'requestAnimationFrame()' means making the animation happen through refreshing the scene every certain time. 
+    window.requestAnimationFrame(animate);
+    
+    
+    //Refresh the context to delete the trace of the movement.
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    background.update();
+    shop.update();
+    player.update();
+    enemy.update();
+
+    player.velocity.x = 0;
+    enemy.velocity.x = 0;
+
+    //(&& lastKey === '') means only the last key you pressed can control the direction, when you press two keys.
+    // player movement
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -5
+    }else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 5
+    }
+   
+
+    // enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5
+    }else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5
+    }
+
+    //@detect for swapping places
+    if (rectangleTurnAround({
+        rectangle1: player,
+        rectangle2: enemy
+    })) {
+        player.attackBox.position.x  = player.position.x - 50;
+        enemy.attackBox.position.x = enemy.position.x;
+    }
+
+    //detect for collision
+    if (rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+    }) && 
+        player.isAttacking &&( gameOver === false)) {
+        player.isAttacking = false;
+        enemy.health -= 20;
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
+    }
+
+    if (rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+    }) && 
+        enemy.isAttacking &&( gameOver === false)) {
+        enemy.isAttacking = false;
+        player.health -= 20;
+        document.querySelector('#playerHealth').style.width = player.health + '%';
+    }  
+    
+
+    // end game based on health
+    if ((player.health <= 0 || enemy.health <= 0) &&( gameOver === false)) {
+        determineWinner({player, enemy, timerId});
+        gameOver = true;
+    }
+     
+}
+animate();
+
+//Listen for the movement of keydown and keyup.
+window.addEventListener('keydown', (event) => {
+
+switch (event.key) {
+    case 'd':
+        keys.d.pressed = true;
+        lastKey = 'd';
+        break;
+    case 'a':
+        keys.a.pressed = true;
+        lastKey = 'a';
+        break;
+    case 'w':
+        player.velocity.y = -10;
+        break;
+    case 's':
+        player.attack();
+        break;
+
+    case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true;
+        enemy.lastKey = 'ArrowLeft';
+        break;
+    case 'ArrowRight':
+        keys.ArrowRight.pressed = true;
+        enemy.lastKey = 'ArrowRight';
+        break;
+    case 'ArrowUp':
+        enemy.velocity.y = -10;
+        break;
+    case 'ArrowDown':
+        enemy.attack();
+        break;
+
+}
+   
+})
+window.addEventListener('keyup', (event) => {
+switch (event.key) {
+    case 'd':
+        keys.d.pressed = false;
+        break;
+    case 'a':
+        keys.a.pressed = false;
+        break;
+    }
+switch (event.key){
+    case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false;
+        break;
+    case 'ArrowRight':
+        keys.ArrowRight.pressed = false;
+        break;
+    }   
+})
+
+
+
+
+
+
---
+++ .history/js/classes_20221121232453.js
@@ -0,0 +1,96 @@
+//Create a class to define the properties and methods. Rendering out the image.
+class Sprite {
+
+    //The position and the velocity are arrays which their values been defined by two directions and 'this' peak to the calling-object.
+    constructor ({position, imageSrc, scale = 1, framesMax = 1}) {
+        //All this properties are independent, can be used by any one.
+        this.position = position;
+        this.width = 50;
+        this.height = 150;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.scale = scale;
+        this.framesMax = framesMax;
+        this.frameCurrent = fra
+    }
+
+    draw () {
+        context.drawImage(
+            this.image, 
+            this.frameCurrent * (this.image.width / this.framesMax),
+            0,
+            this.image.width / this.framesMax, 
+            this.image.height,
+            this.position.x, 
+            this.position.y, 
+            (this.image.width / this.framesMax) * this.scale, 
+            this.image.height * this.scale
+            );        
+    }
+
+    update () {
+        this.draw();          
+    }   
+}
+
+//Duplicate the class of Sprite. The player and the enemy will use it.
+class Fighter {
+
+    //The position and the velocity are arrays which their values been defined by two directions and 'this' peak to the calling-object.
+    constructor ({position, velocity, color, offset}) {
+        //All this properties are independent, can be used by any one.
+        this.position = position;
+        this.velocity = velocity;
+        this.width = 50;
+        this.height = 100;
+        this.lastKey;
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset: offset,
+            width: 100,
+            height: 50,
+        };
+        this.color = color;
+        this.isAttacking;
+        this.health = 100;
+    
+    }
+
+    draw () {
+        context.fillStyle = this.color;        
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        //Attack box
+        if (this.isAttacking){
+        context.fillStyle = 'blue'
+        context.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        }
+    }
+
+    update () {
+
+        //Update the position of the box in real time.
+        this.draw();   
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
+        
+        
+        this.position.x += this.velocity.x ; 
+        this.position.y += this.velocity.y;
+
+        // Avoid the box to fall out of the window.
+        if (this.position.y + this.height + this.velocity.y >= canvas.height - 96){
+            this.velocity.y = 0
+        }else this.velocity.y += gravity; //make the box always be pulled down to the ground.       
+    }
+    //hitting counts once for every touch
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => {
+            this.isAttacking = false
+        },100)
+    }
+}
